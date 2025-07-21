6d69515a85ac4e77b87ed8f39e84088f25830981
ryder <ryderortiz7@gmail.com>
2025-07-21 13:55:45 -0400

    nutritional preference form, styled, exclusive checkboxes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Menu Recommender initialized");
+
+const checkboxes = document.querySelectorAll('.exclusive-checkbox');
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      checkboxes.forEach((otherCheckbox) => {
+        if (otherCheckbox !== this) {
+          otherCheckbox.checked = false;
+        }
+      });
+    }
+  });
+});
